c00150af3476a8c2118859d762ce1e606a47aa16
ThomasWhyyou <thomas.whyyou@ymail.com>
2013-07-07 10:57:19 -0400

    44/280

--- about_strings.rb
+++ about_strings.rb
@@ -2,8 +2,8 @@ require File.expand_path(File.dirname(__FILE__) + '/neo')
 
 class AboutStrings < Neo::Koan
   def test_double_quoted_strings_are_strings
-    string = "Hello, World"
-    assert_equal __, string.is_a?(String)
+    string = "Hello, worstrld"
+    assert_equal true, string.is_a?(String)
   end
 
   def test_single_quoted_strings_are_also_strings
